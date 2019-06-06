b7f1cec59ee72c72f3819553dfc1370edcd0e5f8
rhys <raevanswing36@gmail.com>
2019-06-06 14:56:29 +0100

    edited brush_plots

--- python_scripts/plotting/brush_plots.py
+++ python_scripts/plotting/brush_plots.py
@@ -15,8 +15,9 @@ import seaborn as sns
 
 # Source directory for the files on your local system
 srcdir = '/media/rhys/ExtHD/Project/carlos_peptides/LONG/hydrophilic_brush/helical_brush/'
-figure_path = srcdir+'figures/'
+
 mol = 'EK5_helix'
+figure_path = '{}figures/{}/'.format(srcdir,mol)
 pep_length=10
 num_pep=9
 # default_x = np.linspace(0,2500000.0,num=25001)
@@ -104,24 +105,91 @@ def rolAvgPlot2 (data, window_size, no_of_std, ylims, ylabel, legend, figure_nam
         g.savefig(figure_path + mol + '/' + figure_name + ".png")
 
 ###############################################################################
-#                               DSSP SECTTION
+#                               PROCESSING HELICITY
 ###############################################################################
-#dssp = {'EK5_helix':{}}
-col = ['time']+['pep'+str(x+1) for x in range(num_pep)]
-df = pd.DataFrame(columns=col)
-print(df.head())
-xtc = '{}{m}/06-MD/{m}_final.xtc'.format(srcdir, m=mol)
-topol = '{}{m}/06-MD/{m}_protein.gro'.format(srcdir, m=mol)
 
-for chunk in md.iterload(xtc, 100, top=topol):
-    for i in range(num_pep):
-        peptide_chunk = chunk.atom_slice(chunk.top.select("resid {} to {}".format(i*pep_length, (i+1)*pep_length-1)))
-        dssp = md.compute_dssp(peptide_chunk, simplified=True)
-        helicity = (dssp =='H') | (dssp == 'G') | (dssp == 'I')
-        helicity = np.sum(helicity, axis=1)/1.0
-        print(helicity)
+def processHelicity (srcdir, mol,frames):
+    print("Processing Helicity")
+    col = ['pep'+str(x+1) for x in range(num_pep)]
+    df = pd.DataFrame(columns=col)
+    print(df.head())
+    xtc = '{}{m}/06-MD/{m}_final.xtc'.format(srcdir, m=mol)
+    topol = '{}{m}/06-MD/{m}_protein.gro'.format(srcdir, m=mol)
+
+    for chunk in md.iterload(xtc, 100, top=topol):
+        in_data = np.empty((100,9))
+        for i in range(num_pep): 
+            peptide_chunk = chunk.atom_slice(chunk.top.select("resid {} to {}".format(i*pep_length, (i+1)*pep_length-1)))
+            dssp = md.compute_dssp(peptide_chunk, simplified=True)
+            helicity = (dssp =='H') | (dssp == 'G') | (dssp == 'I')
+            in_data[:,i] = np.sum(helicity, axis=1)/1.0 
+        df = df.append(pd.DataFrame(in_data, columns=col), ignore_index=True)
+    df['tot_hel'] = df.sum(axis=1)
+    print(df.head())
+    print(df.shape)
+    df.iloc[:frames,:].to_csv('./hel_data.csv')
+ 
+
+def processBonds(limit):
+    print("Processing Hydrogen Bonds")
+    arcdir = srcdir+'ARC_files/'
+    # Directoy names
+    sbg = ['EK5_helix']
+    # mode = 'salt_bridges'
+    mode = 'h_bonds'
+    # Defines interval list from which file names are made
+    # segments = np.linspace(0,450,10)
+    segments = np.linspace(0,950,20)
+
+    distances = {'EK5_helix': np.array([]),}
+               # 'E4K4_helix': np.array([]),
+                #'mag_helix': np.array([]),
+               # }
+    coordinates = cp.deepcopy(distances)
+    # For each system...
+    for r in range(len(sbg)): 
+        mol = sbg[r] 
+        # For each interval...
+        for i in range(len(segments)):  
+            lower, upper = int(segments[i])+1, int(segments[i])+50
+            target = "{m}_{l}-{u}_{z}.npy".format(m=mol, l=lower, u=upper, z=mode)  
+    #        chunk = np.load("{s}{m}/h_bonds/{t}".format(s=srcdir, m=mol, t=target))
+            chunk = np.load("{s}{m}/{t}".format(s=arcdir, m=mol, t=target, z=mode))
+            # Add distance/time matrix to dictionary entry for that system
+            if len(distances[mol]) == 0:
+                distances[mol] = chunk
+            else:
+                distances[mol] = np.append(distances[mol], chunk, axis=0)
+            # Set atom:atom pairs (should be the same for every file describing a given system)
+            coordinates[mol] = np.load('{s}{m}/{m}_hyb_atom_order.npy'.format(s=arcdir, m=mol, z=mode))
+            coordinates[mol] = np.subtract(coordinates[mol], -1)
+    binary_contacts = distances[mol] < limit
+    df = pd.DataFrame(np.sum(binary_contacts, 1), columns=['tot_hyb'])
+    df.to_csv('./hyb_data.csv')
+
+###############################################################################
+#                               PLOTTING & RUNNING
+###############################################################################
+processing = [False,False]
+if processing[0]: processHelicity(srcdir,mol,50000)
+elif processing[1]: processBonds(0.28)
+
+else:
+    helicity_data = pd.read_csv('./hel_data.csv', usecols=['tot_hel'])
+    hbond_data = pd.read_csv('./hyb_data.csv', usecols=['tot_hyb'])
+    data = helicity_data.merge(hbond_data,left_index=True,right_index=True)
+    print(data.head())
+    print(data.shape)
+
+    #line_plot = data.plot(y=['tot_hel','tot_hyb'],use_index=True, secondary_y=True)
+    #fig = line_plot.get_figure()
+    #fig.savefig(figure_path+'test3.png')
+
+    data
 
 '''
+
+#################################################################
 helicity = dssp.copy()
 #helicity_per_res = dssp.copy()
 
@@ -136,3 +204,19 @@ for mol in helicity:
 
 '''
 
+
+#plt.plot(EK5_hyb)
+#plt.savefig(figure_path+'test2.png')
+#############################################################################
+'''
+system = 'EK5_helix'
+
+EK5_DSSP_overT = []
+
+for i in range(len(helicity[system])):
+    EK5_DSSP_overT.append(helicity[system][i])
+    
+EK5_DSSP_overT = np.array(EK5_DSSP_overT)
+
+EK5_DSSP = np.sum(EK5_DSSP_overT, 0)
+'''
