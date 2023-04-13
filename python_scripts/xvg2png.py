7f6233d3ab0cb8bd447577498ef31448e0a4adc5
Rhys Evans <rhys.al.evans@gmail.com>
2023-04-13 16:22:58 +0200

    Updated and simplified xvg2png - ahead of Cristina group meeting

--- python_scripts/xvg2png.py
+++ python_scripts/xvg2png.py
@@ -1,87 +1,97 @@
+"""
+===============================================================================
+                        CONVERT XVG FILE TO PNG PLOT
+===============================================================================
+"""
+
 import argparse
-import glob
+from glob import glob, iglob
 import matplotlib.pyplot as plt
 import subprocess
 
-########################################################
-#               GENERATE A-A SEQUENCES
-########################################################
 
-parser = argparse.ArgumentParser(
-                        formatter_class=argparse.RawDescriptionHelpFormatter,
-                        epilog=" ")
-
-parser.add_argument("-f", type=str,
-                    help='File or Folder')
-# optional arguments
-#parser.add_argument("-hisl", type=int, default=3,
-                    #help='Histidine Lenght in block (default: %(default)s)',
-                    #required=False)
-# additional flags
-parser.add_argument("-d", action="store_true",
-                    help='Display once made single only(default: %(default)s)')
-
-args = parser.parse_args()
-PATH = args.f
-DISPLAY = args.d
-
-print(PATH)
-
-if '.xvg' in PATH:
-    print(" INFO | SINGLE FILE CONVERSION")
-    print(f" INFO | Converting: {PATH}")
-    pngname = f"{PATH[:-4]}.png"
-    with open(PATH) as f:
+def convert_file(in_path, out_path):
+    # Read in the xvg file
+    with open(in_path) as f:
         lines = f.readlines()
-    data = [l for l in lines if l[0] not in ("@", "#")]
+    # Filter out comment lines
+    data = [line for line in lines if line[0] not in ("@", "#")]
+    # Convert data to numeric
     data = [[float(val) for val in line.split()[:2]] for line in data]
-    x, y = [l[0] for l in data], [l[1] for l in data]
+    # Seperate columns of data to plot
+    x, y = [line[0] for line in data], [line[1] for line in data]
+    # Create the simplest plot
     plt.plot(x, y)
+    # Extract plot formatting info from the xvg output:
     for line in lines:
+        # Title
         if line[0] == "@" and line.split()[1] == "title":
             plt.title(" ".join(line.split()[2:]).replace('"', ""))
+        # X axis
         if line[0] == "@" and line.split()[1] == "xaxis":
             plt.xlabel(" ".join(line.split()[3:]).replace('"', ""))
+        # Y axis
         if line[0] == "@" and line.split()[1] == "yaxis":
             plt.ylabel(" ".join(line.split()[3:]).replace('"', ""))
-    plt.savefig(pngname, dpi=300)
-    print(f" INFO | Successfully created: {PATH[:-4]}.png")
+    # Save and close the new figure
+    plt.savefig(out_path, dpi=300, bbox_inches='tight')
+    plt.close()
+    print(f" INFO | Successfully created: {pngname}")
 
-    if DISPLAY:
-        try:
-            sub = subprocess.Popen(f"eog {pngname} &",
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT,
-                                   shell=True
-                                   )
-            out, errors = sub.communicate()
-        except:
-            print(' ERROR | unable to display plot')
 
-else:
-    print(" INFO | WHOLE DIRECTORY CONVERSION")
-    print(f" INFO | Converting: {PATH}")
-    pnglist = glob.glob('./{}**/*.png'.format(PATH), recursive=True)
-    for filename in glob.iglob('./{}**/*.xvg'.format(PATH), recursive=True):
-        pngname = f"{filename[:-4]}.png"
-        if pngname in pnglist:
-            print(f" EXST | {pngname}")
-            continue
-        else:
-            print(f" NEW  | {pngname}")
-            with open(filename) as f:
-                lines = f.readlines()
-            data = [l for l in lines if l[0] not in ("@", "#")]
-            data = [[float(val) for val in line.split()[:2]] for line in data]
-            x, y = [l[0] for l in data], [l[1] for l in data]
-            plt.plot(x, y)
-            for line in lines:
-                if line[0] == "@" and line.split()[1] == "title":
-                    plt.title(" ".join(line.split()[2:]).replace('"', ""))
-                if line[0] == "@" and line.split()[1] == "xaxis":
-                    plt.xlabel(" ".join(line.split()[3:]).replace('"', ""))
-                if line[0] == "@" and line.split()[1] == "yaxis":
-                    plt.ylabel(" ".join(line.split()[3:]).replace('"', ""))
-            plt.savefig(pngname)
-            print(f" INFO | Successfully created: {pngname}")
-            plt.close()
+if __name__ == "__main__":
+
+    # Initiate argument parser
+    parser = argparse.ArgumentParser(
+                        formatter_class=argparse.RawDescriptionHelpFormatter,
+                        epilog=" ")
+    # File or directory to process
+    parser.add_argument('-f', type=str,
+                        help='File or Folder')
+    # Optional arguments
+    '''
+    parser.add_argument("-hisl", type=int, default=3,
+                        help='Histidine in block (default: %(default)s)',
+                        required=False)
+    '''
+    # additional flags
+    parser.add_argument('-d', action='store_true',
+                        help=('Display the plot once made - single file only.'
+                              '(default: %(default)s)'))
+    # Take variables from argparser
+    args = parser.parse_args()
+    PATH = args.f
+    DISPLAY = args.d
+
+    # Single file processing
+    if '.xvg' in PATH:
+        print(' INFO | OPERATING MODE - SINGLE FILE CONVERSION')
+        print(f" INFO | Converting: {PATH}")
+        # Define out_path
+        pngname = f"{PATH[:-4]}.png"
+        convert_file(PATH, pngname)
+
+        # Display the file once it has been made
+        if DISPLAY:
+            try:
+                subprocess.run(f"eog {pngname} &", check=True)
+            except subprocess.CalledProcessError as error:
+                print('Error code:', error.returncode,
+                      '. Output:', error.output.decode("utf-8"))
+    # Whole directory processing
+    else:
+        print(' INFO | OPERATING MODE - WHOLE DIRECTORY CONVERSION')
+        print(f" INFO | Converting: {PATH}")
+        # Get the list of existing png files in that directory.
+        pnglist = glob('./{}**/*.png'.format(PATH), recursive=True)
+        for filename in iglob('./{}**/*.xvg'.format(PATH), recursive=True):
+            pngname = f"{filename[:-4]}.png"
+            # Skip any xvg files that have an existing png
+            if pngname in pnglist:
+                print(f" EXST | {pngname}")
+                continue
+            # Process the ones that do not and make new png.
+            else:
+                print(f" NEW  | {pngname}")
+                convert_file(filename, pngname)
+                plt.close()
