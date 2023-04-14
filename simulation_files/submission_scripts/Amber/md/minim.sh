44e6416c04f2b65e79fef0631605b2d3389bdd0c
Rhys Evans <rhys.al.evans@gmail.com>
2023-04-14 17:03:45 +0200

    Corrected and merged Amber Minimisation Scripts, and updated sub_scripts for same process

--- simulation_files/submission_scripts/Amber/md/minim.sh
+++ simulation_files/submission_scripts/Amber/md/minim.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-name=FILE_STEM_HERE
+name="FILE_STEM_HERE"
 
+echo "Starting Minimisation 0"
 sander -O -i min0.in -c ${name}.rst7 -p ${name}.prmtop -o ${name}_min0.out -r ${name}_min0.rst7
 
+echo "Starting Minimisation 1"
 sander -O -i min1.in -c ${name}_min0.rst7 -p ${name}.prmtop -o ${name}_min1.out -r ${name}_min1.rst7
 
+echo "Starting Minimisation 2"
 sander -O -i min2.in -c ${name}_min1.rst7 -p ${name}.prmtop -o ${name}_min2.out -r ${name}_min2.rst7
