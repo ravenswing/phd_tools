23dd25f6c69bad6ed679ff50799a435242cfb902
Rhys Evans <rhys.al.evans@gmail.com>
2024-01-30 15:47:23 +0100

    script organisation WIP

--- python_scripts/load_data.py
+++ python_scripts/load_data.py
@@ -43,33 +43,44 @@ def colvar(filename, output='as_pandas'):
         return old_col.values.astype(float)
 
 
-def fes(filename, is_rew):
-    fes = [[], [], []]
-    with open(filename) as f:
-        lines = f.readlines()
-        data = [s for s in lines if not s.startswith(('@', '#'))]
-    data = [[float(val) for val in ln.split()] for ln in data]
-    breaks = [i for i, e in enumerate(data) if e == []]
-    # Remove blank lines
-    for index in sorted(breaks, reverse=True):
-        del data[index]
-    # Get the number of bins and CV names from header
-    if is_rew:
-        nbins = int(breaks[0])
-        [x_name, y_name] = ['RMSD to IN', 'RMSD to OUT']
+def fes(filename, mode='pandas', is_rew=False):
+    if mode == 'pandas':
+        with open(filename) as f:
+            head = f.readlines()[0]
+        head = head.split()[2:]
+        fes = pd.concat([df for df in pd.read_csv(filename,
+                                                  delim_whitespace=True,
+                                                  names=head,
+                                                  comment='#',
+                                                  chunksize=1000)])
+        return fes
     else:
-        nbins = int([line.split()[-1] for line in lines
-                    if 'nbins_pp.proj' in line][0])
-        [x_name, y_name] = lines[0].split()[2:4]
-    # Organise data into plotable arrays
-    split_data = [data[i:i + nbins] for i in range(0, len(data), nbins)]
-    z = []
-    for block in split_data:
-        z.append([line[2] for line in block])
-        fes[1].append(block[0][1])
-    fes[0] = [line[0] for line in split_data[0]]
-    fes[2] = np.asarray(z)
-    return fes, [x_name, y_name]
+        fes = [[], [], []]
+        with open(filename) as f:
+            lines = f.readlines()
+            data = [s for s in lines if not s.startswith(('@', '#'))]
+        data = [[float(val) for val in ln.split()] for ln in data]
+        breaks = [i for i, e in enumerate(data) if e == []]
+        # Remove blank lines
+        for index in sorted(breaks, reverse=True):
+            del data[index]
+        # Get the number of bins and CV names from header
+        if is_rew:
+            nbins = int(breaks[0])
+            [x_name, y_name] = ['RMSD to IN', 'RMSD to OUT']
+        else:
+            nbins = int([line.split()[-1] for line in lines
+                        if 'nbins_pp.proj' in line][0])
+            [x_name, y_name] = lines[0].split()[2:4]
+        # Organise data into plotable arrays
+        split_data = [data[i:i + nbins] for i in range(0, len(data), nbins)]
+        z = []
+        for block in split_data:
+            z.append([line[2] for line in block])
+            fes[1].append(block[0][1])
+        fes[0] = [line[0] for line in split_data[0]]
+        fes[2] = np.asarray(z)
+        return fes, [x_name, y_name]
 
 
 def fes_simple(filename, is_rew):
