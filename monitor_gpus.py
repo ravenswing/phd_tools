34c8571246f5e8d024d87cb6b024e2d07726b9f8
Rhys Evans <rhys.al.evans@gmail.com>
2024-06-14 15:08:16 +0200

    changes to gpu_monitor

--- monitor_gpus.py
+++ monitor_gpus.py
@@ -12,7 +12,8 @@ users = ["g19sllabres", "g19torces", "g19fjluque", "g19tviayna"]
 def scan_node(n):
     nvidia_out = c.run(f"ssh nodeg{n} -t 'nvidia-smi'", hide="both").stdout.split("\n")
     processes = c.run(
-        f"ssh nodeg{n} -t 'ps axfo user:20,pid,start,comm:20'", hide="both"
+        f"ssh nodeg{n} -t 'ps -axhf -o user:20,pid:10,start:10,command --columns 200'",
+        hide="both",
     ).stdout.split("\n")
 
     nvidia_out = nvidia_out[
@@ -22,9 +23,17 @@ def scan_node(n):
     gpus = pd.DataFrame(nvidia_out, columns=["gpu", "pid", "command"])
 
     processes = [s for s in processes[1:] if s[:4] != "root" and len(s) > 1]
+
     cut_proc = [s for s in processes if s.split()[1] in gpus.pid.to_list()]
+
+    format_proc = [s.replace(" | ", "").replace("\\_ ", "") for s in cut_proc]
+
+    format_proc = [
+        [A[:20].strip(), A[20:31].strip(), A[31:42].strip(), A[42:].strip()]
+        for A in format_proc
+    ]
     users = pd.DataFrame(
-        [s.replace(" | ", "").replace("\\_ ", "").split() for s in cut_proc],
+        format_proc,
         columns=["user", "pid", "start", "ps_command"],
     )
 
@@ -55,7 +64,7 @@ def scan_node(n):
 
 def running_jobs():
     jobs = c.run(
-        "export SGE_ROOT=/sge;/sge/bin/lx24-amd64/qstat -u '*' | grep 'iqtc10_g1819_gpu\|fartorgpu'",
+        "export SGE_ROOT=/sge;/sge/bin/lx24-amd64/qstat -u '*' | grep 'iqtc10_g1819_gpu'",
         hide="both",
     ).stdout.split("\n")
     jobs = [[s.split()[i] for i in (0, 2, 3, 7)] for s in jobs if len(s) > 1]
@@ -70,17 +79,18 @@ def process_df(df, N, jobs):
     for gpu, data in df.groupby("gpu"):
         msg.append(f"\nNodeg{N} - Clash on GPU {gpu}")
         for _, row in data.iterrows():
-            msg.append(f"    {row.user:<12} running {row.command}")
-            msg.append("    Possible Jobs:")
+            msg.append(f"    {row.user}")
+            msg.append(f"        Running:  {row.ps_command}")
+            msg.append("        Possible Jobs:")
             possible_jobs = jobs[(jobs.user == row.user) & (jobs.node == N)]
             for _, pj in possible_jobs.iterrows():
-                msg.append(f"        {pj.jobID:<8} {pj.jobname}...")
+                msg.append(f"            {pj.jobID}  {pj.jobname}...")
 
     return msg
 
 
 def main():
-    output = ["13/06/2024"]
+    output = ["Date: 14/06/2024"]
     for node in range(11, 17):
         clashes = scan_node(node)
         jobs = running_jobs()
